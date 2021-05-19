4971f5c303558ce1e8f7a083e80e1e5b2b77510a
zsolti1012 <56861547+zsolti1012@users.noreply.github.com>
2021-05-19 16:32:03 +0200

    Upload

---
+++ sakkos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace sakkos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sakk sakk1 = new Sakk();
+            Thread t1 = new Thread(sakk1.Kirajzolfelül);
+            Thread t2 = new Thread(sakk1.Kirajzolalul);
+
+            t1.Start();
+            t2.Start();
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ sakkos/Sakk.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sakkos
+{
+    class Sakk
+    {
+        public void Kirajzolfelül()
+        {
+            for (int i = 0; i < 8; i++)
+			{
+                for (int j = 0; j < 4; j++)
+                {
+                    
+                    
+                        if ((i+j)%2!=0)
+                        {
+                            lock (typeof(Console))
+                            {
+                                Console.SetCursorPosition(i, j);
+                                Console.BackgroundColor = ConsoleColor.White;
+                                Console.Write(" ");
+                                System.Threading.Thread.Sleep(10);
+                            }
+
+                        }
+                        
+                    
+                }
+			}
+        }
+
+        public void Kirajzolalul()
+        {
+            for (int i = 7; i >=0; i--)
+            {
+                for (int j = 4; j <8; j++)
+                {
+
+
+                    if ((i + j) % 2 != 0)
+                    {
+                        lock (typeof(Console))
+                        {
+                            Console.SetCursorPosition(i, j);
+                            Console.BackgroundColor = ConsoleColor.White;
+                            Console.Write(" ");
+                            System.Threading.Thread.Sleep(10);
+                        }
+
+                    }
+                   
+
+                }
+            }
+
+        }
+    }
+}
